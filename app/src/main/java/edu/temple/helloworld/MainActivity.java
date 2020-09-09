7ecd82b94effabc587b70b6eb083cc8c2a253f21
Matthew Viola <matthew.viola@temple.edu>
2020-09-09 17:28:55 -0400

    Added button that changes the text label when clicked.

--- app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -3,6 +3,9 @@ package edu.temple.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button clickme = findViewById(R.id.btnClick);
+        clickme.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView main = (TextView) findViewById(R.id.lblMain);
+                main.setText("You have clicked the button");
+            }
+        });
     }
 }
